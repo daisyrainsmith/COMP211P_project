0115e7f4057dabc552859be92f7b9471555f173f
Tom Mo <tom217@foxmail.com>
2017-10-26 11:09:09 +0100

    First upload

---
+++ deliverable1 - tm.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class deliverable1
+{
+   public static void main(String[] args)
+   {
+   
+   Scanner scan = new Scanner(System.in);
+   System.out.println("Welcome to the word game \n"
+                        + "\t\t Login (L) \n"
+                        + "\t\t Register (R) \n"
+                        + "\t\t About (A) \n"
+                        + "\t\t Play the Game (P)\n"
+                        + "\t\t Show the Leader Board (B) \n"
+                        + "\t\t Quit (Q)");
+   
+   String option = scan.nextLine();
+     switch(option) {
+       case L || l:
+       case R || r:
+       case A || a:
+       case P || p:
+       case B || b:
+       case Q || q: }
+       
+       List usernames = ArrayList();
+     
+       System.out.println("Enter your username:");
+       while (usernames[i] = " ") {3
+         username = scan.nextLine;
+         add.usernames(username);
+         i += 1;
+         break; } 
+     
+
+public class player
+     {
+        private ArrayList<String> usernames = ArrayList<String>(); 
+        private ArrayList<String> psasswords = ArrayList<String>();
+
+        public void logname(){
+
+        }
+
+        public void logpassword(){
+
+        }
+
+        public boolean check(){
+
+        }
+
+        
+
+     }
+         
+
+       
+   }
+}
+
+
+

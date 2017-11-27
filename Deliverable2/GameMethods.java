1693c14f1b387a7a31cc56800c9174a9baae99d2
daisyrainsmith <32956494+daisyrainsmith@users.noreply.github.com>
2017-11-27 19:06:03 +0000

    Game Methods is a lot shorter and compiles but not checked further than that yet

--- Deliverable2/GameMethods.java
+++ Deliverable2/GameMethods.java
@@ -16,9 +16,9 @@ public class GameMethods {
     for (int i = 0; i < questionsList.size(); i++) {
       Question theQuestion = questionsList.get(i);
       System.out.println("\nQuestion " + (i+1) + ": ");
-      boolean correctOrNot = runQuestion(theQuestion);
-      resultsList.add(correctOrNot);
-      if (correctOrNot) {
+      boolean correctOrNot = theQuestion.ask();
+      resultsList.add(theQuestion.ask());
+      if (theQuestion.ask()) {
         score++;
       }
     }
@@ -26,69 +26,19 @@ public class GameMethods {
     System.out.println("Final score: " + score); 
     Player.updateScore(score);
     Player.playedAGame();
-  }
-
-  public static boolean runQuestion (Question theQuestion) {
-    Scanner scan = new Scanner(System.in);
-
-    System.out.println("DEBUG: answer is " + theQuestion.getAnswer());
-
-    String[] choices = theQuestion.getWordList();   
-
-    System.out.println("\n" + "Find the word with the meaning of \"" + theQuestion.getQuestion() + "\": ");
-    System.out.print("\nA: " + choices[0]
-                    +"\nB: " + choices[1]
-                    +"\nC: " + choices[2]
-                    +"\nD: " + choices[3]
-                    +"\n\nQuit (Q)"
-                    +"\nSkip (S)");
-
-    char option = 'x';
-    boolean inputError = false; 
-    do {
-      System.out.print("\n\tPlease Enter: ");
-      option = scan.next().charAt(0);
-      switch(option) {
-        case 'A':
-        case 'a':
-          correctOrNot(theQuestion, choices[0]);
-          break;
-        case 'B':
-        case 'b':
-          correctOrNot(theQuestion, choices[1]);
-          break;
-        case 'C':
-        case 'c':
-          correctOrNot(theQuestion, choices[2]);
-          break;
-        case 'D':
-        case 'd':
-          correctOrNot(theQuestion, choices[3]);
-          break;
-        case 'S':
-        case 's':
-          System.out.println("skipped");
-          return false;
-        case 'Q': 
-        case 'q': 
-          System.exit(0);
-          break;
-        default: 
-          System.out.print("Input Error");
-          inputError = true; 
-      } 
-    } while (inputError);
-    return false; 
-  }
-
-  public static boolean correctOrNot(Question theQuestion, String strChoice) {
-    if (theQuestion.isAnswer(strChoice)) {
+    
+  }  
+  public static boolean correctOrNot(Question theQuestion) {
+    if (theQuestion.ask()) {
       System.out.println("correct");
       return true;
     } else {
       System.out.println("wrong");
       return false; 
     } 
+    
+    
+  }
+    
   }
 
-}
